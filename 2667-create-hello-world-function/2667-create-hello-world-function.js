52a7b7264030034ba87deb66abbc1362e5dafa87
Vikash Kumar <vikashkumarjn07@gmail.com>
2026-04-13 09:28:08 +0530

    Time: 40 ms (69.17%), Space: 52.9 MB (86.29%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ * f({}, null, 42); // "Hello World"
+ */
