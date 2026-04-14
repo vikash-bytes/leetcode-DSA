858c228d98fd98b7c5f8be26a25459ab92e2755c
Vikash Kumar <vikash.kumar.cs27@iilm.edu>
2026-04-14 16:11:23 +0530

    Time: 55 ms (5.62%), Space: 54.1 MB (21.1%) - LeetHub

---
+++ 2666-allow-one-function-call/2666-allow-one-function-call.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+    let result;
+
+    return function(...args) {
+        if (!called) {
+            called = true;
+            result = fn(...args);
+            return result;
+        }
+        return undefined;
+    }
+};
+
+/**
+ * Example usage:
+ * let fn = (a,b,c) => (a + b + c);
+ * let onceFn = once(fn);
+ *
+ * console.log(onceFn(1,2,3)); // 6
+ * console.log(onceFn(2,3,6)); // undefined
+ */
