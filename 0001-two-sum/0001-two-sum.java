9932041b193906d32fec1d5f8e07c0af1cb2e8c0
Vikash Kumar <vikash.kumar.cs27@iilm.edu>
2026-04-30 18:22:27 +0530

    Time: 2 ms (99.3%), Space: 47.1 MB (58.33%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            // If complement already exists, return indices
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            // Store current number with its index
+            map.put(nums[i], i);
+        }
+
+        // Problem guarantees one solution, so this line is never reached
+        return new int[] {};
+    }
+}
