7d810dd1a2208ab83c545d78d7c55ccce4b72838
Vikash Kumar <vikash.kumar.cs27@iilm.edu>
2026-04-30 18:24:32 +0530

    Time: 22 ms (90.84%), Space: 90.6 MB (36.88%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                // Check distance between indices
+                if (i - map.get(nums[i]) <= k) {
+                    return true;
+                }
+            }
+            // Update latest index of nums[i]
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
