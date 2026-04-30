c7741e74d92e37f0001d7f2b729a083f94d2bcd3
Vikash Kumar <vikash.kumar.cs27@iilm.edu>
2026-04-30 17:58:16 +0530

    Time: 14 ms (31.6%), Space: 46.3 MB (31.25%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // Convert to lowercase and remove non-alphanumeric characters
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+}
