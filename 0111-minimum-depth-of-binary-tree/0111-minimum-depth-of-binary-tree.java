c5688c6eacb3c66e7e86a33b8241395bbc16979b
Vikash Kumar <vikash.kumar.cs27@iilm.edu>
2026-08-17 12:26:52 +0530

    Time: 5 ms (29.14%), Space: 82.1 MB (56.52%) - LeetHub - Time: O(N), Space: O(N)

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -13,3 +13,8 @@ class Solution {
         return 1 + Math.min(minDepth(root.left), minDepth(root.right));
     }
 }
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
